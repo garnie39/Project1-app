e275a09fbd330a3e7ede8a35bb5ae5d4ffd9c666
garnie39 <kocuri@hotmail.com>
2023-03-26 21:50:27 +1100

    updateJs

--- Project1/Project1.js
+++ Project1/Project1.js
@@ -1,22 +1,26 @@
+let wordGuessed = [[]];
+let space = 1;
 // -- KEYBOARD BUTTON
 const keysButton = document.querySelectorAll(".keyboardRow button");
+// -- LINK THE INPUT FROM KEYBOARD TO THE SQUARES
+function currentWord() {
+  const numWordGuessed = wordGuessed.length;
+  return wordGuessed[numWordGuessed - 1];
+}
+function inputGuessWord(keyButton) {
+  const word = currentWord();
+  if (word && word.length < 5) {
+    word.push(keyButton);
+    // -- LINK THE INPUT FROM KEYBOARD TO THE SQUARES
+    const spaceEl = document.getElementById(String(space));
+    space = space + 1;
+    spaceEl.textContent = keyButton;
+  }
+}
 // -- TO SHOW ALPHABET EACH CLICK
 for (let i = 0; i < keysButton.length; i++) {
   keysButton[i].onclick = ({ target }) => {
     const keyButton = target.getAttribute("data-keyboard");
-    console.log(keyButton);
+    inputGuessWord(keyButton);
   };
-}
-// -- LINK THE INPUT FROM KEYBOARD TO THE SQUARES
-let square = 1;
-function onInput() {
-  if (keyButton === "del") {
-    square -= 1;
-  } else {
-    square++;
-  }
-}
-let id = square;
-function addLetter(id) {
-  document.getElementsById(id).textContent = "keyButton";
 }
